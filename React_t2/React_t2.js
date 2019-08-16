72481d6fec9f1e5757ab1a1ecad112ac19fb4cbe
leipomalla <mari.leipola@gmail.com>
2019-08-16 10:24:03 +0300

    t2 toimii osittain

--- React_t2/React_t2.js
+++ React_t2/React_t2.js
@@ -1,3 +1,22 @@
 //You have to make the character to run by displaying the images runner1, runner2, runner3 and
 //runner4 with a speed of 60ms.
 
+
+
+var imageArray = ["runner1.png", "runner2.png","runner3.png", "runner4.png"];
+console.log(imageArray.length);
+
+setInterval(vaihdaKuvaa, 2000);
+
+function vaihdaKuvaa() {
+    for (i = 1; i < imageArray.length; i++) {
+       document.getElementById("kuva").src = imageArray[i];
+    }
+}
+
+
+
+
+
+
+
